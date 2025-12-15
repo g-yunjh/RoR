895531c3139e769d01abde27a7f44176a4035562
g-yunjh <yunjh1126@naver.com>
2025-12-15 14:39:27 +0900

    간단한 유효성 검사

--- my_first_app/app/controllers/welcome_controller.rb
+++ my_first_app/app/controllers/welcome_controller.rb
@@ -1,9 +1,11 @@
 class WelcomeController < ApplicationController
   def submit
-    @username = params[:username]
-    @message = params[:message]
-
-    # 폼 데이터 처리 로직 (데이터베이스 저장 등)
-    render 'result' # 결과 페이지로 이동
+    @user = User.new(name: params[:username], email: params[:email], age: params[:age])
+    if @user.valid?
+      @user.save # 데이터베이스에 저장
+      redirect_to users_path # 성공 후 사용자 목록 페이지로 이동
+    else
+      render 'new' # 유효성 검사 실패 시 폼을 다시 보여주기
+    end
   end
 end
