8bc199202174a49903f835e0a185893147be6edb
g-yunjh <yunjh1126@naver.com>
2025-12-15 15:28:19 +0900

    간단한 앱 만들기

--- my_first_app/app/controllers/welcome_controller.rb
+++ my_first_app/app/controllers/welcome_controller.rb
@@ -1,11 +1,5 @@
-class WelcomeController < ApplicationController
-  def submit
-    @user = User.new(name: params[:username], email: params[:email], age: params[:age])
-    if @user.valid?
-      @user.save # 데이터베이스에 저장
-      redirect_to users_path # 성공 후 사용자 목록 페이지로 이동
-    else
-      render 'new' # 유효성 검사 실패 시 폼을 다시 보여주기
+  class WelcomeController < ApplicationController
+    def index
+      # 홈페이지 로직
     end
   end
-end
--- my_first_app/config/routes.rb
+++ my_first_app/config/routes.rb
@@ -1,15 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root 'welcome#index' # 홈페이지
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
-  # Defines the root path route ("/")
-  root 'welcome#index'
-  post '/submit', to: 'welcome#submit'
+  get '/about', to: 'pages#about' # 소개 페이지
+  get '/contact', to: 'pages#contact' # 연락처 페이지
+  get '/posts', to: 'posts#index' # 게시글 목록 페이지
+  get '/users', to: 'users#index' # 사용자 목록 페이지
 end
