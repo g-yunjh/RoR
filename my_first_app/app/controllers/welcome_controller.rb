0aa61dc1c4a28b1f00d758fa4fa7a99db49cf3a5
g-yunjh <yunjh1126@naver.com>
2025-12-15 13:11:57 +0900

    받은 데이터 화면에 출력하기

--- my_first_app/app/controllers/welcome_controller.rb
+++ my_first_app/app/controllers/welcome_controller.rb
@@ -1,4 +1,9 @@
-     class WelcomeController < ApplicationController
-       def index
-       end
-     end
+class WelcomeController < ApplicationController
+  def submit
+    @username = params[:username]
+    @message = params[:message]
+
+    # 폼 데이터 처리 로직 (데이터베이스 저장 등)
+    render 'result' # 결과 페이지로 이동
+  end
+end
